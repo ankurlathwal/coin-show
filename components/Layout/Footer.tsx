1687279a5b89c9c3ffb951aa043b8a65b7837be0
Ankur Lathwal <ankur@Monikas-MacBook-Pro.local>
2021-09-06 23:12:17 +1000

    Add: basic layout and tailwind theme

---
+++ components/Layout/Footer.tsx
@@ -0,0 +1,6 @@
+import { FC } from 'react';
+
+const Footer: FC = () => {
+  return <footer>Powered by CoinGecko API</footer>;
+};
+export default Footer;
---
+++ components/Layout/Nav.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react';
+
+const Nav: FC = () => {
+  return <nav>Navigation</nav>;
+};
+
+export default Nav;
